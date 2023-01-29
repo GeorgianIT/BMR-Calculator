5f688d2fc604806c4eb9285c62fed5bef3c84416
GeorgianIT <iulian.tudor2405@gmail.com>
2023-01-29 15:36:10 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import BMRform from './components/BMRform';
+
+const style = {
+  page: 'flex bg-neutral-300',
+  content: 'w-[60vw] flex flex-col justify-start m-auto my-[10vh] bg-neutral-300 h-[80vh]',
+  title: 'font-bold text-center text-[50px]',
+  description: 'text-[25px]',
+  tablediv: 'flex justify-center',
+  recommandation: 'mt-10 text-center text-[20px]'
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.page}>
+      <div className={style.content}>
+        <p className={style.title}>BMR calculator</p>
+        <p className={style.description}>The Basal Metabolic Rate (BMR) Calculator estimates your basal metabolic rate—the amount of energy expended while at rest in a neutrally temperate environment </p>
+        <div className={style.tablediv}>
+          <BMRform />
+          
+        </div>
+        <p className={style.recommandation}>Generally, the recommended daily calorie intake is 2,000 calories a day for women and 2,500 for men.</p>
+      </div>
     </div>
   );
 }
